7295d7e517f408bd9171225cebbca19ab1172df5
rtsubaki00 <rinaaaaaaaaaa0811@gmail.com>
2020-06-02 15:48:17 +0900

    投稿機能

--- site.php
+++ site.php
@@ -1,17 +1,19 @@
 <?php
-$name = $_POST['name'];
-$job = $_POST['job'];
-$text = $_POST['text'];
+$name = $_POST['name'];
+$job = $_POST['job'];
+$text = $_POST['text'];
 ?>
-<!DOCTYPE html>
-<html>
+
+<!DOCTYPE html>
+<html lang="ja">
 <head>
-    <meta http-equiv="content-type" content="text/html; charset=Shift-JIS">
-    <title>結果</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>結果</title>
 </head>
 <body>
-    <p>名前：<?php echo $name; ?></p><br>
+    <p>名前：<?php echo $name; ?></p><br>
     <p>職業：<?php echo $job; ?></p><br>
-    <p>本文：<?php echo $text ?></p>
+    <p>本文：<?php echo $text; ?></p>
 </body>
 </html>
