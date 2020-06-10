728517054347b5033711ea6a225623f6778fd973
rtsubaki00 <rinaaaaaaaaaa0811@gmail.com>
2020-06-10 18:57:46 +0900

    最低限の投稿機能

--- site2.php
+++ site2.php
@@ -17,7 +17,7 @@ $_SESSION['body'] = $body;
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <title>確認フォーム</title>
+    <title>確認</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
