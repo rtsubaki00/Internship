659e5e8e2969925874905b5c782b253a68d6c9ce
rtsubaki00 <rinaaaaaaaaaa0811@gmail.com>
2020-06-14 22:28:54 +0900

    途中

--- question4.php
+++ question4.php
@@ -1,9 +1,24 @@
 <?php
 session_start();
 
-$text = htmlspecialchars($_POST['body'], ENT_QUOTES, 'UTF-8');
+$text = htmlspecialchars($_SESSION['text'], ENT_QUOTES, 'UTF-8');
 
-$_SESSION['text'] = $text;
+$dsn = 'mysql:host=localhost; dbname=hellohhelo; charset=utf8';
+$user = 'hellouser';
+$password = 'rpass';
+
+try{
+    $db = new PDO($dsn, $user, $password);
+    $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = 'INSERT INTO question(body) VALUES ("'.$text.'")';
+
+    $stmt = $db -> prepare($sql);
+} catch (PDOException $e) {
+    exit ('エラー:' .$e -> getMessage());
+}
+
+$stmt -> execute();
 ?>
 
 <!DOCTYPE html>
@@ -11,14 +26,12 @@ $_SESSION['text'] = $text;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>確認フォーム</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>完了</title>
 </head>
 <body>
-    <form action="question4.php" method="post">
-    　<p>内容:<?php echo $text; ?></p>
-
-    <button type="submit" class="btn btn-primary ml-3">送信</button>
+    <p>登録完了</p>
+    <form action="question1.php" method="post">
+        <button type="submit" class="btn btn-primary ml-3">トップページに戻る</button>
     </form>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
